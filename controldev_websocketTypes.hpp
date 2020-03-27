81d644770613f22c1d22a94a7c515a49c9880fee
Henrique Ferreira <henrique.ferreira@tidewise.io>
2020-03-27 17:49:40 -0300

    initial commit by rock-create-orogen

---
+++ controldev_websocketTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef controldev_websocket_TYPES_HPP
+#define controldev_websocket_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace controldev_websocket {
+}
+
+#endif
+
